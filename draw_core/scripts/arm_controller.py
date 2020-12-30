87f1877f419881f654c7cee7cf837a10e5348e18
Roger Xu <xuyanran.edu@gmail.com>
2020-12-30 15:08:16 +0800

    track point

--- draw_core/scripts/arm_controller.py
+++ draw_core/scripts/arm_controller.py
@@ -12,7 +12,7 @@ import geometry_msgs.msg
 from std_msgs.msg import String
 
 class Arm_Contrl:
-    def __init__(self):
+    def __init__(self, P_A, P_B):
         moveit_commander.roscpp_initialize(sys.argv)     
         rospy.init_node('drawing_control', anonymous = True)
 
@@ -20,20 +20,23 @@ class Arm_Contrl:
         self.group_name = "manipulator_i5"
         self.group = moveit_commander.MoveGroupCommander(self.group_name)
 
+        # for real world settings
+        self.height = 0.15
+
         # initial settings
         reference = String()
         reference = "world"
         self.group.set_pose_reference_frame(reference)
         self.group.allow_replanning(True)
-        self.group.set_max_velocity_scaling_factor(0.4)      # TODO: What for?
+        self.group.set_max_velocity_scaling_factor(0.4)      
         self.group.set_max_acceleration_scaling_factor(0.3)
-        self.group.set_goal_orientation_tolerance(0.1)
-        self.group.set_goal_position_tolerance(0.1)
-        self.group.set_planning_time(3.0)
+        self.group.set_goal_orientation_tolerance(0.02)
+        self.group.set_goal_position_tolerance(0.02)
+        self.group.set_planning_time(6.0)
 
         # set target A and B
-        # self.A = A
-        # self.B = B
+        self.A = P_A
+        self.B = P_B
 
     def move(self, goal_state):
         # get current state
@@ -44,23 +47,26 @@ class Arm_Contrl:
 
         # plan and execute
         plan = self.group.go(wait = True)
-        self.group.stop()
+        time.sleep(5.0)
+        # self.group.stop()
         self.group.clear_pose_targets()
-        time.sleep(1.0)
+        
 
 
     def controller(self): 
+        self.move(self.A)
+        
         # move done
         # get current state    
         current_state = geometry_msgs.msg.Pose()
         current_state = self.group.get_current_pose().pose   
-        # print(current_state)                          
+        print(current_state)                          
 
         # set target state
         goal_state = geometry_msgs.msg.Pose()
         goal_state.position.x = current_state.position.x
         goal_state.position.y = current_state.position.y
-        goal_state.position.z = current_state.position.z + 0.1
+        goal_state.position.z = current_state.position.z - self.height
         goal_state.orientation.x = current_state.orientation.x
         goal_state.orientation.y = current_state.orientation.y
         goal_state.orientation.z = current_state.orientation.z
@@ -69,9 +75,7 @@ class Arm_Contrl:
         self.move(goal_state)
         # end move done
 
-        # self.move(self.A)
-
-        # self.move(self.B)
+        self.move(self.B)
 
         # move up 
         # get current state    
@@ -82,7 +86,7 @@ class Arm_Contrl:
         goal_state = geometry_msgs.msg.Pose()
         goal_state.position.x = current_state.position.x
         goal_state.position.y = current_state.position.y
-        goal_state.position.z = current_state.position.z - 0.1
+        goal_state.position.z = current_state.position.z + self.height
         goal_state.orientation.x = current_state.orientation.x
         goal_state.orientation.y = current_state.orientation.y
         goal_state.orientation.z = current_state.orientation.z
@@ -93,7 +97,27 @@ class Arm_Contrl:
 
 
 if __name__=="__main__":
-    control = Arm_Contrl()
+    # test goal
+    P_A = geometry_msgs.msg.Pose()
+    P_A.position.x = -0.22
+    P_A.position.y = -0.01
+    P_A.position.z = 1.26
+    P_A.orientation.x = 0.69
+    P_A.orientation.y = -0.00
+    P_A.orientation.z = -0.00
+    P_A.orientation.w = 0.71
+
+    P_B = geometry_msgs.msg.Pose()
+    P_B.position.x = -0.42
+    P_B.position.y = -0.41
+    P_B.position.z = 1.26
+    P_B.orientation.x = 0.69
+    P_B.orientation.y = -0.00
+    P_B.orientation.z = -0.00
+    P_B.orientation.w = 0.71 
+    
+    # end test
+    control = Arm_Contrl(P_A, P_B)
     control.controller()
 
 
