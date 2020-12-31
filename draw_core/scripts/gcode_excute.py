1127fcb0173a13cc9ebba9509d930373ab57fd37
Roger Xu <xuyanran.edu@gmail.com>
2020-12-31 15:31:02 +0800

    import my_controller

--- draw_core/scripts/gcode_excute.py
+++ draw_core/scripts/gcode_excute.py
@@ -1,4 +1,5 @@
 import numpy as np
+import arm_controller
 
 class gcode_excute():
     def __init__(self, xi, yi, x0, y0):
@@ -8,16 +9,21 @@ class gcode_excute():
 
         self.amax = 0.3
 
+        # instanlize controller
+        self.controller = Arm_Contrl()
+
     def G0(self, x, y): # the "just go there" instruction
         xn, yn = self.tf(x, y)
         print "G0",x,y
         # self.path = line.Line(self.x, self.y, xn, yn, 0.5, self.amax)
+        self.controller.draw_line(x, y)
 
     def G1(self, x, y, fr): # move to x, y in a line at a speed in mm/minute
         fr /= 1000 * 60 # mm/minute -> m/sec
         xn, yn = self.tf(x, y)
         print "G1",x,y
         # self.path = line.Line(self.x, self.y, xn, yn, fr, self.amax)
+        self.controller.draw_line(x, y)
 
     def G2(self, x, y, xc, yc, fr): # move in a clockwise arc to an endpoint around a center
         fr /= 1000 * 60 # mm/minute -> m/sec
